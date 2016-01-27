7a10887cfe009f3489cbc1c4f2c7999bbd31ace6
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2016-01-27 08:53:08 -0800

    Added a Emscripten C benchmark.

---
+++ tests/emscripten_c/fibonacci.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+	int fibonacci(int n) {
+		if (n < 2) {
+			return n;
+		}
+		return fibonacci(n - 2) + fibonacci(n - 1);
+	}
+
+int main() {
+	int retval = fibonacci(43);
+	printf("fib: %d\n", retval);
+	return 0;
+}
