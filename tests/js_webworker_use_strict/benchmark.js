e8627336b7fa63a779c709cb099b0aa632a4c22a
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2016-01-27 04:17:21 -0800

    Added as JavaScript 'use strict' benchmark.

---
+++ tests/js_webworker_use_strict/benchmark.js
@@ -0,0 +1,22 @@
+
+
+function benchmarkJSWebWorkerUseStrict(cb) {
+	var worker = new Worker('worker.js');
+	worker.onmessage = function(e) {
+		switch (e.data.action) {
+			case 'end':
+				var start_time = e.data.start_time;
+				var result = e.data.result;
+				var end_time = e.data.end_time;
+				var diff_time = end_time - start_time;
+				cb(diff_time, result);
+				worker.terminate();
+				break;
+		}
+	};
+
+	var message = {
+		action: 'start'
+	};
+	worker.postMessage(message);
+}
---
+++ tests/js_webworker_use_strict/worker.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+self.addEventListener('message', function(e) {
+	var fibonacci = function(n) {
+		"use strict";
+		if (n < 2) {
+			return n;
+		}
+		return fibonacci(n - 2) + fibonacci(n - 1);
+	};
+
+	var message = {
+		action: 'end',
+		start_time: new Date().getTime(),
+		result: fibonacci(43),
+		end_time: new Date().getTime()
+	};
+	self.postMessage(message);
+}, false);
