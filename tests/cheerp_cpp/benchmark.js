8d9b3f0a4b248e1b5d07e89fac2cf14feb6e26fb
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2016-01-28 22:16:17 -0800

    Moved Cheerp benchmark into WebWorker.

--- tests/cheerp_cpp/benchmark.js
+++ tests/cheerp_cpp/benchmark.js
@@ -1,16 +1,47 @@
 
-var jsBridge = null;
+
 
 function benchmarkCheerpCPP(cb) {
+	var currentScriptPath = function() {
+		// NOTE: document.currentScript does not work in a Web Worker
+		// So we have to parse a stack trace maually
+		try {
+			throw new Error('');
+		} catch(e) {
+			var stack = e.stack;
+			var line = null;
+
+			// Chrome and IE
+			if (stack.indexOf('@') !== -1) {
+				line = stack.split('@')[1].split('\n')[0];
+			// Firefox
+			} else {
+				line = stack.split('(')[1].split(')')[0];
+			}
+			line = line.substring(0, line.lastIndexOf('/')) + '/';
+			return line;
+		}
+	};
+
 	console.info('benchmarkCheerpCPP');
+	var path = currentScriptPath() + 'worker.js';
+	console.info(path);
+	var worker = new Worker(path);
+	worker.onmessage = function(e) {
+		switch (e.data.action) {
+			case 'end':
+				var start_time = e.data.start_time;
+				var result = e.data.result;
+				var end_time = e.data.end_time;
+				var diff_time = end_time - start_time;
+				cb(diff_time, result);
+				worker.terminate();
+				break;
+		}
+	};
 
-	if(! jsBridge) {
-		jsBridge = new JsBridge();
-	}
-
-	var start_time = new Date().getTime();
-	var result = jsBridge.fibonacci(43);
-	var end_time = new Date().getTime();
-	var diff_time = end_time - start_time;
-	cb(diff_time, result);
+	var message = {
+		action: 'start'
+	};
+	worker.postMessage(message);
 }
--- tests/cheerp_cpp/worker.js
+++ tests/cheerp_cpp/worker.js
@@ -1,19 +1,18 @@
 
 
-
+var jsBridge = null;
 
 self.addEventListener('message', function(e) {
-	var fibonacci = function(n) {
-		if (n < 2) {
-			return n;
-		}
-		return fibonacci(n - 2) + fibonacci(n - 1);
-	};
+	importScripts('cheerp_main.js');
+
+	if(! jsBridge) {
+		jsBridge = new JsBridge();
+	}
 
 	var message = {
 		action: 'end',
 		start_time: new Date().getTime(),
-		result: fibonacci(43),
+		result: jsBridge.fibonacci(43),
 		end_time: new Date().getTime()
 	};
 	self.postMessage(message);
