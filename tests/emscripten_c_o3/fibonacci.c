16aac261c17ea889596aa88237090721ae89f992
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2016-01-28 20:48:25 -0800

    Cleanup

--- tests/emscripten_c_o3/fibonacci.c
+++ tests/emscripten_c_o3/fibonacci.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h>
 
-	int fibonacci(int n) {
-		if (n < 2) {
-			return n;
-		}
-		return fibonacci(n - 2) + fibonacci(n - 1);
+int fibonacci(int n) {
+	if (n < 2) {
+		return n;
 	}
+	return fibonacci(n - 2) + fibonacci(n - 1);
+}
 
 int main() {
 	int retval = fibonacci(43);
